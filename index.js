f82765ed3fbcf6b1a305b150ef9818f8f52f8608
nightfury1122 <imam50312@gmail.com>
2024-07-21 18:26:39 +0500

    reducer setup

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ function orderCake() {
         quantity: 1,
     }
 }
+
+// initial state  
+const initialState = {
+
+    numOfCakes : 10,
+}
+
+// a reducer is what actually changes the state an action tells it to 
+// a reducer required only two arguments 1. action 2. the previous state  
+
+const reducer = (state = initialState , action)=>{
+
+    switch(action.type){
+        case CAKE_ORDERD:
+            return{
+                numOfCakes: state.numOfCakes -1,
+
+            }
+            default:
+                return state
+    }
+
+}
