6a22eaf4e9602ce38c2382c7937f7318c6272663
nightfury1122 <imam50312@gmail.com>
2024-07-21 18:27:43 +0500

    code formatted

--- index.js
+++ index.js
@@ -13,22 +13,25 @@ function orderCake() {
 // initial state  
 const initialState = {
 
-    numOfCakes : 10,
+    numOfCakes: 10,
+    anotherProperty: 0,
 }
 
 // a reducer is what actually changes the state an action tells it to 
 // a reducer required only two arguments 1. action 2. the previous state  
 
-const reducer = (state = initialState , action)=>{
+const reducer = (state = initialState, action) => {
 
-    switch(action.type){
+    switch (action.type) {
         case CAKE_ORDERD:
-            return{
-                numOfCakes: state.numOfCakes -1,
+            return {
+                // normally the state contains multiple items, we should make a copy using the spread operator and then update the required item
+                ...state,
+                numOfCakes: state.numOfCakes - 1,
 
             }
-            default:
-                return state
+        default:
+            return state
     }
 
 }
