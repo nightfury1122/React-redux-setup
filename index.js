e84684e6bb966cf2428eaad606466d6be955a654
nightfury1122 <imam50312@gmail.com>
2024-07-21 18:15:18 +0500

    basic action and action creator made

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('From index.js')
+// a string that defined the type of action 
+const CAKE_ORDERD = "CAKE_ORDERD"
+
+// a simple Action wrapped in an action creator
+
+function orderCake() {
+    return {
+        type: CAKE_ORDERD,
+        quantity: 1,
+    }
+}
